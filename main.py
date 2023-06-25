0ddaf9987a20d8d4727ca717ca173eba184d50ce
bar07865 <137136679+bar07865@users.noreply.github.com>
2023-06-25 06:04:16 +0000

    assignment 1

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 def main():
-    """
-    ##################################################
-    Comlete your code here
-    Use m_perc and f_perc for your results
-    ##################################################
-    """
+    
+    males = int(input("Enter the number of males:"))
+    females = int(input("Enter number of females:"))
+    total = males + females 
+    m_perc = (males / total) * 100
+    f_perc = (females / total) * 100
 
-    """
-    ########################################
-    # Do not delete the return statement
-    ########################################
-    """
+    print("Total number of students: \t ", total)
+    print("The number of males and females: \t ", males , females )
+    print("Percentage of males and females: \t ", m_perc,"%", f_perc,"%" )
     return m_perc, f_perc
 
 
